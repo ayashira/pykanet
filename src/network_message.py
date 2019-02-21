f14b6fbc3ed4250a7ba577328ab8a6d41bbb5dd8
simon0203 <none>
2019-02-21 22:30:06 +0900

    minor bug correction (class name forgotten in front of constant) + code cleanup of unit tests + unit tests of messages with void subparts

--- src/network_message.py
+++ src/network_message.py
@@ -35,39 +35,52 @@ class Network_Message():
     
     #read each subpart of the message from a byte encoding
     def from_bytes(self, complete_message):
-        start_idx = message_prefix_size
+        start_idx = Network_Message.message_prefix_size
         subpart_list = []
-        nb_subparts = len(subpart_prefix_size)
+        nb_subparts = len(Network_Message.subpart_prefix_size)
         for i in range(nb_subparts):
             subpart_length = int.from_bytes(complete_message[start_idx:start_idx+Network_Message.subpart_prefix_size[i]], byteorder='big')
             start_idx += Network_Message.subpart_prefix_size[i]
             subpart_list.append( complete_message[start_idx:start_idx+subpart_length].decode('utf-8') )
             start_idx += subpart_length
         
+        #initialize the class variables with the message subparts
         self.username, self.network_path, self.network_command, self.message_content = subpart_list
 
+def check_message_equality(message_a, message_b):
+    if message_a.username != message_b.username:
+        print("FAILED: username")
+        return False
+    
+    if message_a.network_path != message_b.network_path:
+        print("FAILED: network_path")
+        return False
+    
+    if message_a.network_command != message_b.network_command:
+        print("FAILED: network_command")
+        return False
+    
+    if message_a.message_content != message_b.message_content:
+        print("FAILED: message_content")
+        return False
+        
+    return True
+
+#check that we obtain the same message after converting to bytes and converting back from bytes
+def check_identity(*message_arguments):
+    message_a = Network_Message(*message_arguments)
+    message_b = Network_Message()
+    message_b.from_bytes( message_a.to_bytes() )
+    if check_message_equality(message_a, message_b):
+        print("Test... OK")
+    
 #perform unit tests if the module was not imported
 #TODO : more unit tests, especially for exception handling of "bad" messages
 if __name__ == '__main__':
-    message_test = Network_Message("user1", "/user/user1/userpage", "ADD", "A new message from user1")
-    message_copy = Network_Message()
-    message_copy.from_bytes( message_test.to_bytes() )
-    if message_copy.username != message_test.username:
-        print("FAILED: username")
-    else:
-        print("OK: username")
-        
-    if message_copy.network_path != message_test.network_path:
-        print("FAILED: network_path")
-    else:
-        print("OK: network_path")
-
-    if message_copy.network_command != message_test.network_command:
-        print("FAILED: network_command")
-    else:
-        print("OK: network_command")
-
-    if message_copy.message_content != message_test.message_content:
-        print("FAILED: message_content")
-    else:
-        print("OK: message_content")
+    #Test that we obtain the same message after converting to bytes and converting back from bytes
+    check_identity("user1", "/user/user1/userpage", "ADD", "A new message from user1")
+    check_identity("", "/user/user1/userpage", "ADD", "A new message from user1")
+    check_identity("user1", "", "ADD", "A new message from user1")
+    check_identity("user1", "/user/user1/userpage", "", "A new message from user1")
+    check_identity("user1", "/user/user1/userpage", "ADD", "")
+    check_identity("", "", "", "")
