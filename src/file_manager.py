306ef498d62abdbbba2e385abaa1fe9f00c8817d
simon0203 <none>
2019-03-10 18:18:21 +0900

    FileManager : server files saved in a directory pykanet_data

--- src/file_manager.py
+++ src/file_manager.py
@@ -1,12 +1,27 @@
 
 import hashlib
+import os
 
 #Interface to the host file system
 #All "file accesses" should be done through this class
 #It will allow in the future to cache data, and also reorganize it and store it efficiently 
 class FileManager():
+    
+    #should be called only once when application is started
+    #return True if initialization was successful
+    def init_save_path():
+        FileManager.root_dir = "pykanet_data/"
+        if not os.path.exists(FileManager.root_dir):
+            try:
+                os.makedirs(FileManager.root_dir)
+            except:
+                #save directory does not exist and could not be created
+                return False
+        
+        return True
+    
     def file_read(network_path):
-        filename = hashlib.sha224(network_path.encode('utf-8')).hexdigest()
+        filename = FileManager.root_dir + hashlib.sha224(network_path.encode('utf-8')).hexdigest()
         try:
             with open(filename) as file:
                 return file.read()
@@ -17,7 +32,7 @@ class FileManager():
     #TODO: for efficiency, some cache mechanism could be used
     #return True if writing was successful
     def file_write(network_path, new_content):
-        filename = hashlib.sha224(network_path.encode('utf-8')).hexdigest()
+        filename = FileManager.root_dir + hashlib.sha224(network_path.encode('utf-8')).hexdigest()
         try:
             with open(filename, "w") as file:
                 file.write(new_content)
@@ -28,7 +43,7 @@ class FileManager():
     
     #TODO: for efficiency, some cache mechanism could be used
     def file_append(network_path, added_content):
-        filename = hashlib.sha224(network_path.encode('utf-8')).hexdigest()
+        filename = FileManager.root_dir + hashlib.sha224(network_path.encode('utf-8')).hexdigest()
         try:
             with open(filename, "a") as file:
                 file.write(added_content)
--- src/main_server.py
+++ src/main_server.py
@@ -5,15 +5,25 @@ from twisted.internet import protocol
 
 from message_passing_protocol import MessagePassingProtocol
 from server_services import ServerServices
+from file_manager import FileManager
 
 port_to_listen = 8883
 
 def main():
+    #initialize file saving directories
+    if not FileManager.init_save_path():
+        #file saving not working correctly, we should not run the server
+        print("FileManager initialization... [FAILED]. Stopping.")
+        return
+    else:
+        print("FileManager initialization... [OK]")
+        
     factory = protocol.ServerFactory()
     factory.protocol = MessagePassingProtocol
     factory.is_server = True
     factory.server_services = ServerServices()
     
+    print("Server started...")
     reactor.listenTCP(port_to_listen,factory)
     reactor.run()
 
