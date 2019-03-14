ae8a53f842ba9f6da4c5af5c13a7d5ef6a612af0
simon <none>
2019-03-14 22:57:52 +0900

    serialization : support of lists, including nested lists

--- src/serialization_utils.py
+++ src/serialization_utils.py
@@ -26,7 +26,17 @@ class Serialize():
         complete_message = bytearray(b'')
     
     #convert some value to bytes and add it to some data buffer
-    def write_value(buffer, val):
+    def write_value(buffer, val):        
+        #recursive types
+        if type(val) is list:
+            #encode first the length of the list, and then each value separately
+            buffer += Serialize.types_list[str(type(val))].to_bytes(Serialize.TYPE_PREFIX_LENGTH, byteorder='big')
+            buffer += (len(val)).to_bytes(Serialize.SIZE_PREFIX_LENGTH, byteorder='big')
+            for elmt in val:
+                Serialize.write_value(buffer, elmt)
+            return
+        
+        #basic types
         if type(val) is str:
             value_bytes = val.encode('utf-8')
             buffer += Serialize.types_list[str(type(val))].to_bytes(Serialize.TYPE_PREFIX_LENGTH, byteorder='big')
@@ -44,6 +54,17 @@ class Serialize():
         start_idx += Serialize.TYPE_PREFIX_LENGTH
         data_length = int.from_bytes(buffer[start_idx:start_idx+Serialize.SIZE_PREFIX_LENGTH], byteorder='big')
         start_idx += Serialize.SIZE_PREFIX_LENGTH
+        
+        #recursive types
+        if data_type == Serialize.DATA_LIST_TYPE:
+            #read each element and append them to the list
+            result_list = []
+            for _ in range(data_length):
+                elmt, start_idx = Serialize.read_value(buffer, start_idx)
+                result_list.append(elmt)
+            return result_list, start_idx
+        
+        #basic types
         if data_type == Serialize.DATA_STR_TYPE:
             val = buffer[start_idx:start_idx+data_length].decode('utf-8')
         elif data_type == Serialize.DATA_INT_TYPE:
@@ -65,16 +86,47 @@ class Serialize():
 def test_identity(value):
     new_value = Serialize.from_bytes( Serialize.to_bytes(value) )
     if new_value != value:
-        print("Identity FAILED", value)
+        print("Identity FAILED", value, new_value)
 
 if __name__ == '__main__':
     #Test that we obtain the same value after serialization/deserialization
+    
+    #strings
     test_identity("")
     test_identity("a")
     test_identity("abcdefghij")
     test_identity("test\n\n\t\\\\test")
+    
+    #numbers
     test_identity(0)
     test_identity(123450)
     test_identity(11111111111112222222222222233333333333333)
+    
+    #negative numbers
+    #test_identity(-21)
+    
+    #booleans
     test_identity(True)
     test_identity(False)
+    
+    #list of same elements
+    test_identity([])
+    test_identity([""])
+    test_identity(["aaaa"])
+    test_identity(["aaaa", "bbbb"])
+    test_identity(["abc", "defghy", "", "ertetrer", "a", "", "dfg", "df", "df"])
+    test_identity([12, 345, 543525])
+    test_identity([121212121, 121212345, 12543525, 0, 0, 1, 12121, 3244])
+    test_identity([True, False, True, True, True, True, False])
+    
+    #mixed list
+    test_identity(["abc", 123, 32444444444444444444444444, True, True, 344])
+    
+    #list of list
+    test_identity([ [12, 12], [123, 234, 345], [0, 1, 2, 3, 4] ])
+    test_identity([[]])
+    test_identity([ [], [1], [2, 3], [4, 5, 6], [], [], ["abc", "def"] ])
+    
+    #multiple levels of list
+    test_identity([ [1, 2, [3, 4, [2], [ [34, ""], "aa" ]], [[[[[[1, 2], [[[]]] ]]]]]  ], [[[], 3], 4], 5 ])
+    test_identity([[[[[[[[[[[[[[[[[[[[[[[[[1, 2], 3], True], False, False], []], 1]]]], 1]]], 3]], 4]]]]]]]]]]])
