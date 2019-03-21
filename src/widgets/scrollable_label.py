299d878f43aad202b13b5e5a0a126c957ed55472
simon0203 <none>
2019-03-21 23:30:32 +0900

    code organization: CustomLabel of apps/chat_client.py moved to widgets/scrollable_label.py

--- src/widgets/scrollable_label.py
+++ src/widgets/scrollable_label.py
@@ -3,6 +3,8 @@ from kivy.uix.scrollview import ScrollView
 from kivy.properties import StringProperty, ListProperty
 from kivy.lang import Builder
 
+from kivy.uix.boxlayout import BoxLayout
+
 #for regular expressions
 import re
 
@@ -53,6 +55,62 @@ class ScrollableLabel(ScrollView):
         
         return text_str
         
+    def link_clicked(self, link):
+        if link.startswith("http"):
+            import webbrowser
+            webbrowser.open(link)
+        else:
+            self.dispatch('on_link_clicked', link)
+
+    def on_link_clicked(self, link):
+        pass
+
+#format the links in some text string, with the markup language of kivy
+#TODO : code redundancy with ScrollableLabel
+def format_links(text_str):
+    #use a regular expression to add kivy color and ref markup around web addresses
+    text_str = re.sub(r'(https?:\S*)', r'[color=0000ff][u][ref=\1]\1[/ref][/u][/color]', text_str, flags=re.MULTILINE)
+    return text_str
+
+    
+Builder.load_string('''
+<CustomLabel>:
+    size_hint_x: 0.8
+    size_hint_y: None
+    height: self.minimum_height
+    orientation: "vertical"
+    
+    Label:
+        id:minor_label
+        size_hint: None, None
+        size: self.texture_size
+        markup:True
+        text: ""
+        pos_hint: {'left': 1}
+    Label:
+        id:text_label
+        size_hint_y: None
+        height: self.texture_size[1]
+        text_size: self.width, None
+        padding: [7, 7]
+        markup:True
+        on_ref_press: root.link_clicked(args[1])
+        canvas.before:
+            Color:
+                rgba: root.bcolor
+            RoundedRectangle:
+                pos: self.pos
+                size: self.size
+''')
+
+#default text to None, default background to white
+class CustomLabel(BoxLayout):
+    
+    #add an event triggered when a link other than http link is clicked
+    __events__ = BoxLayout.__events__ + ('on_link_clicked',)
+    
+    bcolor = ListProperty([1,1,1,1])
+    
     def link_clicked(self, link):
         if link.startswith("http"):
             import webbrowser
