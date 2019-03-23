e65617cced61498bbdb193faf21bfc6fc71d183c
simon0203 <none>
2019-03-23 17:53:00 +0900

    comment format (space after #)

--- src/games/connectfour.py
+++ src/games/connectfour.py
@@ -1,10 +1,12 @@
-# Game of Connect Four
-#rules reference: https://en.wikipedia.org/wiki/Connect_Four
 
 class ConnectFour():
+    '''
+        Game of Connect Four
+        rules reference: https://en.wikipedia.org/wiki/Connect_Four
+    '''
     
     def __init__(self):
-        #0 = empty, 1 = occupied by player 1, 2 = occupied by player 2 
+        # 0 = empty, 1 = occupied by player 1, 2 = occupied by player 2 
         self.board = [[0 for y in range(self.rows())] for x in range(self.cols())]
         self.current_player = 1
     
@@ -14,11 +16,11 @@ class ConnectFour():
     def cols(self):
         return 7
     
-    #for display : width and height of a cell when displaying the game
+    # for display : width and height of a cell when displaying the game
     def cell_size(self):
         return 50, 50
     
-    #for display: label for cell at coordinates (x, y)
+    # for display: label for cell at coordinates (x, y)
     def get_label(self, x, y):
         s = self.board[x][y]
         if s == 0:
@@ -32,17 +34,17 @@ class ConnectFour():
         #TODO
         pass
     
-    #update the board with the move from a player
+    # update the board with the move from a player
     def play(self, move, player):
         #TODO
         
-        #update the current_player
+        # update the current_player
         self.current_player = 2 if self.current_player == 1 else 1
 
     def get_current_player(self):
         return self.current_player
     
-    #must return -1 if game not finished, 0 if draw, 1 or 2 if player 1 or 2 has won
+    # must return -1 if game not finished, 0 if draw, 1 or 2 if player 1 or 2 has won
     def winner(self):
         #TODO
         pass
--- src/games/reversi.py
+++ src/games/reversi.py
@@ -1,5 +1,8 @@
-# Game of Reversi
-#rules reference: https://en.wikipedia.org/wiki/Reversi
+'''
+    Game of Reversi
+    rules reference: https://en.wikipedia.org/wiki/Reversi
+'''
+
 import numpy as np
 import itertools
 
@@ -9,11 +12,11 @@ class Reversi():
         self._rows=rows
         self._cols=cols
         
-        #0 = empty, 1 = occupied by player 1, 2 = occupied by player 2 
+        # 0 = empty, 1 = occupied by player 1, 2 = occupied by player 2 
         self.board  = np.zeros((self.rows(), self.cols()))
         self.current_player = 1
         
-        #set the initial state(Othello opening)
+        # set the initial state(Othello opening)
         pos = (self.rows()//2-1, self.rows()//2)
         for p1, p2 in itertools.product(pos, pos):
             self.board[p1][p2] = ((p1-pos[0])^(p2-pos[0]))+1
@@ -24,11 +27,11 @@ class Reversi():
     def cols(self):
         return self._cols
     
-    #for display : width and height of a cell when displaying the game
+    # for display : width and height of a cell when displaying the game
     def cell_size(self):
         return 40, 40
     
-    #for display: label for cell at coordinates (x, y)
+    # for display: label for cell at coordinates (x, y)
     def get_label(self, x, y):
         s = self.board[x][y]
         if s == 0:
@@ -42,7 +45,7 @@ class Reversi():
         x, y  = move
         return len(self.__flip_discs(x, y, player)) > 0
     
-    #update the board with the move from a player
+    # update the board with the move from a player
     def play(self, move, player):
         x, y = move
         fd = self.__flip_discs(x, y, player)
@@ -51,8 +54,8 @@ class Reversi():
             for ix, iy in fd:
                 self.board[ix][iy] = player
 
-        #update the current_player
-        #same player plays again if opponent has no possible move
+        # update the current_player
+        # same player plays again if opponent has no possible move
         next_player = 2 if self.current_player == 1 else 1
         if self._play_possible(next_player):
             self.current_player = next_player
@@ -60,17 +63,17 @@ class Reversi():
     def get_current_player(self):
         return self.current_player
     
-    #returns -1 if game is not finished, 0 if draw, 1 or 2 if player 1 or 2 has won
+    # returns -1 if game is not finished, 0 if draw, 1 or 2 if player 1 or 2 has won
     def winner(self):
         blank = np.sum(self.board == 0)
         if blank > 0:
-            #if any of the player can still play, game is not finished
+            # if any of the player can still play, game is not finished
             next_player = 2 if self.current_player == 1 else 1
             if self._play_possible(next_player) or self._play_possible(self.current_player):
                 return -1
         
-        #game is finished
-        #the winner is the player with most discs
+        # game is finished
+        # the winner is the player with most discs
         point_1 = np.sum(self.board == 1)
         point_2 = np.sum(self.board == 2)
         
@@ -79,13 +82,13 @@ class Reversi():
         elif point_1 < point_2:
             return 2
         else:
-            #point_1 == point_2, this is a draw
+            # point_1 == point_2, this is a draw
             return 0
     
     def __flip_discs(self, x, y, player):
-        #list of possible straight 8 directions through the board
-        #one direction is represented by the move variation on x and y axis
-        #note : it also includes a useless "no move" direction 
+        # list of possible straight 8 directions through the board
+        # one direction is represented by the move variation on x and y axis
+        # note : it also includes a useless "no move" direction 
         increment_funcs = (lambda x:x, lambda x:x+1, lambda x:x-1)
         directions = itertools.product(increment_funcs, increment_funcs)
         
@@ -127,9 +130,9 @@ class Reversi():
         
         return False
 
-#tests
+# tests
 if __name__ == '__main__':
-    #check that a player cannot play
+    # check that a player cannot play
     reversi = Reversi(4, 4)
     reversi.board[0] = np.ones(4)*1
     reversi.board[1] = np.ones(4)*1
