53240f62f24f6be8c10850c663e85a59e29bf4c7
simon0203 <none>
2019-03-24 21:25:04 +0900

    Cryptography: wrapper for elliptic curve cryptography. Uses internally python 'cryptography' library (pip install needed).

--- src/user_utils.py
+++ src/user_utils.py
@@ -1,34 +1,108 @@
-import hashlib
+'''
+    Classes to mnage users and user authentification
+    
+    User authentification uses Elliptic Curve cryptography.
+    cryptography python library needs to be installed.
+    Installation with pip: pip install cryptography
+'''
 
-# note: used only temporarily for dummy implementation
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives import serialization
+
+# note: used only in the unit tests to generate random strings
 import random
 
 class CryptUtil():
     '''
-        Public-key encryption
-        For now, this is a dummy class just to provide the interface
-        Internal functions will be replaced by calls to a cryptographic library
+        Wrapper for Elliptic curve cryptography
+        This class uses the "cryptography" python library.
     '''
     
-    # generate a random public/private key
-    # for this dummy implementation, public=private
     def generate_key():
-        private_key = hashlib.sha256(str(random.randint(1,1000000000)).encode('utf-8')).hexdigest()
-        # this is just to have different private/public keys
-        public_key = private_key + "xyz"
+        '''
+            Generate a random public/private key
+        '''
+        
+        # default_backend() is in fact the only backend existing in cryptography library
+        # see: https://cryptography.io/en/latest/hazmat/backends/
+        # so there is no risk of different default_backend() between users
+        private_key = ec.generate_private_key(ec.SECP384R1(), default_backend())
+        public_key = private_key.public_key()
         return public_key, private_key
     
-    # sign a message with a private key
     def sign_message(private_key, message):
-        hash = hashlib.sha256(message.encode('utf-8')).hexdigest()
-        return hashlib.sha256( (private_key + hash).encode('utf-8')).hexdigest()
+        '''
+            Sign a message with a private key
+        '''
+        
+        data = message.encode('utf-8')
+        signature = private_key.sign(data,ec.ECDSA(hashes.SHA256()))
+        return signature
+        
+    def check_sign(public_key, message, signature):
+        '''
+            Check the signature of a message with a public key
+        '''
+        
+        data = message.encode('utf-8')
+        try:
+            public_key.verify(signature, data, ec.ECDSA(hashes.SHA256()))
+        except:
+            return False
+        return True
+
+    def serialize_public(public_key):
+        '''
+            Return an ascii-encoded string of the public key in the PEM format
+        '''
+        
+        serialized_public = public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+        return serialized_public
+
+    def load_public(serialized_public_key):
+        '''
+            Reconstructs a public key from its serialized encoding
+        '''
+        
+        public_key = serialization.load_pem_public_key(
+            serialized_public_key,
+            backend=default_backend()
+        )
+        return public_key
     
-    # check the signature of a message with a public key
-    def check_sign(public_key, message, message_sign):
-        hash = hashlib.sha256(message.encode('utf-8')).hexdigest()
-        # remove the last 3 characters of the public key in order to obtain the private key
-        signature = hashlib.sha256( (public_key[:-3] + hash).encode('utf-8')).hexdigest()
-        return signature == message_sign
+    def serialize_private(private_key, password):
+        '''
+            Return an ascii-encoded string of the private key
+            The private key is encrypted with the given password
+        '''
+        
+        # TODO : BestAvailableEncryption is not guaranteed to be the same over time or between users
+        #        We should specify a fixed symmetric cipher instead
+        
+        serialized_private = random_private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.BestAvailableEncryption(password.encode('utf-8'))
+        )
+        return serialized_private
+    
+    def load_private(serialized_private_key, password):
+        '''
+            Reconstructs a private key from its serialized encoding
+            The same password used during serialization is needed.
+        '''
+        
+        private_key = serialization.load_pem_private_key(
+            serialized_private_key,
+            password=password.encode('utf-8'),
+            backend=default_backend()
+        )
+        return private_key
 
 # singleton-class to manage the main logged user from any part of the program
 class MainUser():
@@ -55,8 +129,18 @@ if __name__ == '__main__':
     #   check that signature is NOT correct with first public key
     loop_nb = 1000
     fail_nb = 0
-    for _ in range(loop_nb):
+    for i in range(loop_nb):
         random_public_key, random_private_key = CryptUtil.generate_key()
+        serialized_public_key = CryptUtil.serialize_public(random_public_key)
+        serialized_private_key = CryptUtil.serialize_private(random_private_key, "testpassword")
+        
+        #if i <= 10:
+        #    print(serialized_public_key)
+        #    print(serialized_private_key)
+        
+        random_public_key = CryptUtil.load_public(serialized_public_key)
+        random_private_key = CryptUtil.load_private(serialized_private_key, "testpassword")
+        
         random_message = str(random.randint(1,1000000000))
         sign = CryptUtil.sign_message(random_private_key, random_message)
         if not CryptUtil.check_sign(random_public_key, random_message, sign):
